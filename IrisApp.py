84593c5d4269563843c1fe368bd325256c967710
Rand <63405866+Rand-RB@users.noreply.github.com>
2022-05-18 20:15:43 +0300

    Add files via upload

---
+++ IrisApp.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 17:27:54 2022
+
+@author: ranood
+"""
+import streamlit as st
+import pickle
+from PIL import Image
+import pandas as pd
+
+#load the model
+km = pickle.load(open('Kmeans_Classifier.pkl', 'rb'))
+
+#load the imges
+setosa= Image.open('setosa.png')
+versicolor= Image.open('versicolor.png')
+virginica = Image.open('virginica.png')
+
+
+st.write("""
+         # IRIS Classificatin App
+         
+### Let's Predict the Flowers Type Based on Some Features by Using Machine Learning
+         
+         """)
+         
+#read the data from the user
+st.sidebar.header("User Input Features")
+def user_input():
+    sep_l = st.sidebar.slider("sepal_length", 0.0, 1.0)
+    sep_w = st.sidebar.slider("sepal_width", 0.0, 1.0)
+    pet_l = st.sidebar.slider("petal_length", 0.0, 1.0)
+    pet_w = st.sidebar.slider("petal_width", 0.0, 1.0)
+    
+    features = {"sepal_length": sep_l, "sepal_width":sep_w,
+                "petal_length":pet_l, "petal_width":pet_w}
+    data = pd.DataFrame(features, index=[0])
+    print(data)
+    return data
+
+user_input = user_input() 
+
+if st.button("Click Here to Predict"):
+    pred = km.predict(user_input)
+    
+st.image(setosa) if pred == 0 else st.image(versicolor)  if pred == 1 else st.image(virginica)
+
+
+#print the user input
+st.subheader("User Input")
+st.write(user_input)
+
+
+
+
+
+
+#read the model 
+load_model = pickle.load(open('myModel2.pkl', 'rb'))
